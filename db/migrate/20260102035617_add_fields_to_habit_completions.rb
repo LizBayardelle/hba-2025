0038d8ad2ec14772e103ba50a0645fcc5e583dc9
Liz Bayardelle <lizbayardelle@gmail.com>
2026-01-01 20:25:50 -0800

    Fix migration to check for existing columns

--- db/migrate/20260102035617_add_fields_to_habit_completions.rb
+++ db/migrate/20260102035617_add_fields_to_habit_completions.rb
@@ -1,11 +1,11 @@
 class AddFieldsToHabitCompletions < ActiveRecord::Migration[7.2]
   def change
-    add_reference :habit_completions, :user, null: false, foreign_key: true
-    add_column :habit_completions, :count, :integer, default: 1, null: false
-    add_column :habit_completions, :streak_count, :integer, default: 0
+    add_reference :habit_completions, :user, null: false, foreign_key: true unless column_exists?(:habit_completions, :user_id)
+    add_column :habit_completions, :count, :integer, default: 1, null: false unless column_exists?(:habit_completions, :count)
+    add_column :habit_completions, :streak_count, :integer, default: 0 unless column_exists?(:habit_completions, :streak_count)
 
     change_column :habit_completions, :completed_at, :date, null: false
 
-    add_index :habit_completions, [:habit_id, :completed_at], unique: true
+    add_index :habit_completions, [:habit_id, :completed_at], unique: true unless index_exists?(:habit_completions, [:habit_id, :completed_at])
   end
 end
